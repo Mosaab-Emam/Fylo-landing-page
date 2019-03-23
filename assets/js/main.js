46593dd55adb1e614b94f39b3f71a72ca9e7ff35
Mosaab Emam <mosaab.emam123@gmail.com>
2019-03-23 15:06:54 +0300

    Responsified subscribe section

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,25 @@ const toggleNavMenu = () => {
         navbar.classList.remove('expand');
     }
 }
+
+// Fix subscribe section position
+function centeriseSub() {
+    const testimonials = document.getElementById('testimonials');
+    const sub = document.getElementById('subscribe');
+    const body = document.getElementsByTagName('body')[0];
+    const footer = document.getElementsByTagName('footer')[0];
+    const subHeight = sub.clientHeight;
+    const subWidth = sub.clientWidth;
+    const bodyWidth = body.clientWidth;
+    
+    // Centrise the subscribe section vertically
+    sub.style.marginTop = (-subHeight / 2) + 'px';
+    sub.style.left = ((bodyWidth / 2) - (subWidth / 2)) + 'px';
+
+    // Set up appropriate paddings for testimonials section and footer
+    testimonials.style.paddingBottom = (subHeight / 2) + 20 + 'px';
+    footer.style.paddingTop = (subHeight / 2) + 20 + 'px';
+}
+
+centeriseSub();
+window.addEventListener('resize', centeriseSub);
